c740d003c0f8c2af9ceafd9f516c41f68439fc76
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2026-01-16 16:47:53 +0530

    Create Car_Bike.java

---
+++ Java/Abstraction/src/Abstraction/Car_Bike.java
@@ -0,0 +1,13 @@
+package Abstraction;
+
+public class Car_Bike {
+
+	public static void main(String[] args) {
+		Vehicle v1 = new Car();
+		Vehicle v2 =new Bike();
+		
+		v1.run();
+		v2.run();
+	}
+
+}
