9f79baab8762498df33151c458e5c3bac5afdee0
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2026-01-16 16:47:42 +0530

    Create Gpay.java

---
+++ Java/Abstraction/src/Abstraction/Gpay.java
@@ -0,0 +1,8 @@
+package Abstraction;
+
+public abstract class Gpay {
+	public abstract double check_Balance();
+	public abstract double deposit(double amount);
+	public abstract double withdraw(double amount);
+	
+}
