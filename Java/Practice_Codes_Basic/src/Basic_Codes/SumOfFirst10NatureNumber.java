c3ecd357d9cf6a030b38eea239a94de3ce8830ab
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2026-01-19 16:28:37 +0530

    Create SumOfFirst10NatureNumber.java

---
+++ Java/Practice_Codes_Basic/src/Basic_Codes/SumOfFirst10NatureNumber.java
@@ -0,0 +1,13 @@
+package Basic_Codes;
+import java.util.*;
+public class SumOfFirst10NatureNumber {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1 ;i<=10;i++) {
+			sum=sum+i;
+		}
+		System.out.print("Sum of first 10 nature number : "+sum);
+	}
+
+}
