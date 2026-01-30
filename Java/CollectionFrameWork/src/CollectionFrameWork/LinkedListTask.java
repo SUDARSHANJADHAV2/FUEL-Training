56ea49ccfe71d058f6e00b7ed857f95f44c7f461
SUDARSHAN SANTAJI JADHAV <sudarshanjadhav2@zohomail.in>
2026-01-30 19:15:59 +0530

    Update LinkedListTask.java

--- Java/CollectionFrameWork/src/CollectionFrameWork/LinkedListTask.java
+++ Java/CollectionFrameWork/src/CollectionFrameWork/LinkedListTask.java
@@ -4,7 +4,6 @@ import java.util.LinkedList;
 public class LinkedListTask {
     public static void main(String[] args) {
 
-        // Create LinkedList of Employee objects
         LinkedList<Employee> empList = new LinkedList<>();
 
         // Add employees
@@ -12,7 +11,6 @@ public class LinkedListTask {
         empList.add(new Employee(102, "Anita", 42000));
         empList.add(new Employee(103, "Suresh", 50000));
 
-        // Calculate sum of salaries
         double totalSalary = 0;
 
         for (Employee e : empList) {
